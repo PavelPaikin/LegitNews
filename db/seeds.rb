83cb8f1f31827da36a7b2845e1a21d07730b6245
Pavel Paikin <pavel.paikin2@stclairconnect.ca>
2018-03-01 19:47:05 -0500

    Preparing for application deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+for i in 1..50
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  name = "#{first_name} #{last_name}"
+  email = Faker::Internet.free_email(name)
+
+  user = User.new
+  user.name = name
+  user.email = email
+
+  if user.save
+    article = Article.new
+    article.title = Faker::Lorem.sentence
+    article.content = Faker::Lorem.paragraph
+    article.user = user
+
+    article.save
+
+  end
+
+
+end
+
+Article.all.each do |article|
+  User.all.each do |user|
+    for ii in 1..10
+      comment = Comment.new
+      comment.message = Faker::Lorem.paragraph
+      comment.user = user
+      comment.article = article
+
+      comment.save
+    end
+  end
+end
+
